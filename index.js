2c4a292ea0bb847a244be21cd97bb26bace9e213
usershruti13 <103852967+usershruti13@users.noreply.github.com>
2023-10-10 16:25:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const container= document.getElementById("container");
+const nodetext= document.getElementById("node");
+const addNodeButton= document.getElementById("add-node");
+const addChildButton= document.getElementById("add-child");
+const addParentButton= document.getElementById("add-parent");
